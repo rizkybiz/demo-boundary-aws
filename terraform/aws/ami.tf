a0643fc5d9a9db3217e1c98a5fbf89c47846acbf
rizkybiz <justin.defrank@hashicorp.com>
2021-11-22 16:51:12 -0500

    Fixed an issue where the default ubuntu AMI had gotten deleted

--- terraform/aws/ami.tf
+++ terraform/aws/ami.tf
@@ -31,3 +31,19 @@ data "aws_ami" "vault" {
 
   owners = ["${var.aws_ami_owner}"]
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
